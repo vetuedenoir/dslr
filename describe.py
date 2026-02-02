613ed1e9a190fa57285e83d5db7fac30ed89f897
killian <killianscordel@gmail.com>
2026-02-02 22:35:02 +0100

    partie 1 finis, describe.py norme et op

--- describe.py
+++ describe.py
@@ -5,11 +5,11 @@ import numpy as np
 from math import sqrt
 
 
-class   TinyStatistician():
+class TinyStatistician():
     def __init__(self):
         pass
-    
-    staticmethod
+
+    @staticmethod
     def mean(array):
         if not isinstance(array, (list, set, tuple, np.ndarray)):
             print("Not the good type")
@@ -24,52 +24,51 @@ class   TinyStatistician():
                 return None
             resulte += element
         resulte = resulte / len(array)
-        return resulte
-    
+        return float(resulte)
 
-    staticmethod
+    @staticmethod
     def median(array):
-        if not isinstance(array, (list, set, tuple)):
+        if not isinstance(array, (list, set, tuple, np.ndarray)):
             return None
         if isinstance(array, (set, tuple)):
             array = list(array)
-        l = len(array)
-        if l == 0:
+        lenght = len(array)
+        if lenght == 0:
             return None
-        if l == 1:
+        if lenght == 1:
             return array[0]
-        for element in  array:
+        for element in array:
             if not isinstance(element, (int, float)):
                 return None
         array.sort()
-        if l % 2 != 0:
-            return float(array[int((l / 2))])
-        elif l >= 1:
-            return float((array[int(l / 2) - 1] + array[int((l / 2) )]) / 2)
+        if lenght % 2 != 0:
+            return float(array[int((lenght / 2))])
+        elif lenght >= 1:
+            m1 = array[int(lenght / 2) - 1]
+            m2 = array[int((lenght / 2))]
+            return float((m1 + m2) / 2)
         return float(array[0])
-    
 
-    staticmethod
+    @staticmethod
     def quartile(array):
-        if not isinstance(array, (list, set, tuple)):
-                return None
+        if not isinstance(array, (list, set, tuple, np.ndarray)):
+            return None
         if isinstance(array, (set, tuple)):
             array = list(array)
-        l = len(array)
-        if l == 0:
+        lenght = len(array)
+        if lenght == 0:
             return None
-        if l == 1:
+        if lenght == 1:
             return float(array[0])
-        for element in  array:
+        for element in array:
             if not isinstance(element, (int, float)):
                 return None
         array.sort()
-        # first, third = 0.0
-        quart = l / 4
+
+        quart = lenght / 4
         return [float(array[int(quart)]), float(array[int(quart * 3)])]
 
-
-    staticmethod
+    @staticmethod
     def percentile(array, p):
         """
         Calcule le pᵉ percentile d'une liste de valeurs,
@@ -83,17 +82,14 @@ class   TinyStatistician():
             float | None: La valeur du percentile, ou None si entrée invalide.
         """
 
-        if not isinstance(array, (list, set, tuple)):
+        if not isinstance(array, (list, set, tuple, np.ndarray)):
             return None
-
         if isinstance(array, (set, tuple)):
             array = list(array)
-
         if len(array) == 0:
             return None
         if not (0 <= p <= 100):
             return None
-
         array = sorted(array)
         n = len(array)
         if n == 1:
@@ -113,13 +109,11 @@ class   TinyStatistician():
         lower_value = array[lower_index]
         upper_value = array[upper_index]
         result = lower_value + fraction * (upper_value - lower_value)
-
         return float(result)
-    
 
-    staticmethod
+    @staticmethod
     def var(array):
-        if not isinstance(array, (list, set, tuple)):
+        if not isinstance(array, (list, set, tuple, np.ndarray)):
             return None
 
         if isinstance(array, (set, tuple)):
@@ -128,49 +122,48 @@ class   TinyStatistician():
             return None
         if len(array) == 1:
             return float(array[0])
-        mean = mean(array)
-        if mean == None:
+        m = TinyStatistician.mean(array)
+        if m is None:
             return None
-        ecart = [x - mean for x in array]
+        ecart = [x - m for x in array]
         ecart_carre = [x * x for x in ecart]
         somme_carre = 0
         for x in ecart_carre:
             somme_carre += x
         result = somme_carre / (len(array) - 1)
         return float(result)
-    
 
-    staticmethod
+    @staticmethod
     def std(array):
         v = TinyStatistician.var(array)
-        if v == None:
+        if v is None:
             return None
         return sqrt(v)
-    
-    staticmethod
+
+    @staticmethod
     def min(array):
-        if not isinstance(array, (list, set, tuple)):
+        if not isinstance(array, (list, set, tuple, np.ndarray)):
             return None
         if isinstance(array, (set, tuple)):
             array = list(array)
-        
-        return min(array)
-    
-    staticmethod
+
+        return float(min(array))
+
+    @staticmethod
     def max(array):
-        if not isinstance(array, (list, set, tuple)):
+        if not isinstance(array, (list, set, tuple, np.ndarray)):
             return None
         if isinstance(array, (set, tuple)):
             array = list(array)
-        
-        return max(array)
 
-
+        return float(max(array))
 
 
 def parse():
     parser = argparse.ArgumentParser(prog="describe")
-    parser.add_argument("data_file", type=str, help="The data_set file with extension csv")
+    parser.add_argument("data_file",
+                        type=str,
+                        help="The data_set file with extension csv")
     return parser.parse_args()
 
 
@@ -185,46 +178,69 @@ def read_csv(file: str):
 def describe_feature(data_feature):
     description = {}
 
-
     description["Count"] = len(data_feature)
     description["Mean"] = TinyStatistician.mean(data_feature)
     description["Std"] = TinyStatistician.std(data_feature)
     description["Min"] = TinyStatistician.min(data_feature)
-    description["25%"] = TinyStatistician.quartile(data_feature)
+    description["25%"] = TinyStatistician.percentile(data_feature, 25.0)
     description["50%"] = TinyStatistician.percentile(data_feature, 50.0)
     description["75%"] = TinyStatistician.percentile(data_feature, 75.0)
     description["Max"] = TinyStatistician.max(data_feature)
-
     return description
 
 
+def process_features(data_frame):
+    features_name = [
+                    "Arithmancy",
+                    "Astronomy",
+                    "Herbology",
+                    "Defense Against the Dark Arts",
+                    "Divination",
+                    "Muggle Studies",
+                    "Ancient Runes",
+                    "History of Magic",
+                    "Transfiguration",
+                    "Potions",
+                    "Care of Magical Creatures",
+                    "Charms",
+                    "Flying"]
 
-def process_features(data):
-    features = ["Arithmancy", "Astronomy", "Herbology", "Defense Against the Dark Arts", "Divination", "Muggle Studies",
-                "Ancient Runes", "History of Magic", "Transfiguration", "Potions", "Care of Magical Creatures", "Charms", "Flying"]
-    
-    description_off_all_features = {}
+    infos_features = {}
 
-    for f in  features:
-        description_off_all_features[f] = describe_feature(data[f].to_numpy(dtype=float).astype(np.float32))
-    
-    return description_off_all_features
+    for f in features_name:
+        feature = data_frame[f].dropna().to_numpy(dtype=float)
+        infos_features[f] = describe_feature(feature)
+
+    return infos_features
 
 
 def display_information(info):
-    names_of_feature = "\t"
+    features_name = list(info.keys())
 
-    for t in info:
-        names_of_feature += f"\t  {t}"
-    print(names_of_feature)
-    for t in info:
-        print(t, "\t\t", info[t])
-       
+    truncate_features_name = []
+    for f in features_name:
+        if len(f) > 13:
+            truncate_features_name.append(f[:13] + '.')
+        elif len(f) < 8:
+            truncate_features_name.append(f + '\t')
+        else:
+            truncate_features_name.append(f)
+    header = "\t" + "\t".join(truncate_features_name)
+    print(header)
+
+    stats_order = ["Count", "Mean", "Std", "Min", "25%", "50%", "75%", "Max"]
+
+    for stat in stats_order:
+        line = stat
+        for feature in features_name:
+            line += f"\t{info[feature][stat]:5f}"
+        print(line)
+
 
 def main():
     args = parse()
-    data_tab = read_csv(args.data_file)
-    features_description =  process_features(data_tab)
+    data_frame = read_csv(args.data_file)
+    features_description = process_features(data_frame)
     display_information(features_description)
 
 
