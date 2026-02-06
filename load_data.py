Fix d'erreurs + ajouts de normalisation

load_data.py
@@ -2,23 +2,32 @@ import numpy as np
 import pandas as pd
 from data_visualization.pair_plot import to_keep
 
-def load_data():
+def load_data(path : str):
     """load the data, keep only the courses with heterogene repartition of data
     return numpy arrays, to the model"""
-    data = pd.read_csv("assets/dataset_train.csv")
+    data = pd.read_csv(path)
 
-    courses = to_keep()
-    data_num = data[courses]
+    courses = to_keep(path)
+    data = data[courses].dropna()
+    data_num = data[courses].dropna()
+
+    courses.remove("Hogwarts House")
     data_num = data_num.drop(['Hogwarts House'], axis="columns")
-    #attention pas sur
-    data_num = data_num.dropna()
-    
-    huff_y = (data['Hogwarts House'] == 'Hufflepuff').to_numpy(dtype=int)
-    gryf_y = (data['Hogwarts House'] == 'Gryffindor').to_numpy(dtype=int)
-    sly_y = (data['Hogwarts House'] == 'Slytherin').to_numpy(dtype=int)
-    rav_y = (data['Hogwarts House'] == 'Ravenclaw').to_numpy(dtype=int)
 
-    return data, huff_y, gryf_y, sly_y,rav_y
+    data_num = data_num.to_numpy(dtype=float)
+    
+    min_val = np.min(data_num, axis=0)
+    max_val = np.max(data_num, axis=0)
+    new_data_num = (data_num - min_val) / (max_val - min_val)
+    print(new_data_num)
+    print(np.max(new_data_num, axis=0))
+    
+    huff_y = (data['Hogwarts House'] == 'Hufflepuff').dropna().to_numpy(dtype=float).reshape(data_num.shape[0], 1)
+    gryf_y = (data['Hogwarts House'] == 'Gryffindor').dropna().to_numpy(dtype=float).reshape(data_num.shape[0], 1)
+    sly_y = (data['Hogwarts House'] == 'Slytherin').dropna().to_numpy(dtype=float).reshape(data_num.shape[0], 1)
+    rav_y = (data['Hogwarts House'] == 'Ravenclaw').dropna().to_numpy(dtype=float).reshape(data_num.shape[0], 1)
+
+    return data_num, huff_y, gryf_y, sly_y, rav_y
 
 
 if __name__ == "__main__":

logreg_train.py
@@ -4,24 +4,8 @@ import matplotlib.pyplot as plt
 import sys
 import argparse
 
-import LogisticRegression as lr
-
-
-def load_data(file: str) -> pd.DataFrame:
-    """
-    Load the data_set and return the interisting features as pd.DataFrame
-    """
-    df = pd.read_csv(file)
-    if df is None:
-        print(f"Cannot open the file: {file}")
-        sys.exit(1)
-    #charger les features interessant.
-    #
-    #
-    return df
-    
-    #le seul optimizer facile a mettre en place c'est adam
-    #A voir si on le met par default 
+from LogisticRegression import LogisticRegression as lr
+from load_data import load_data
 
 def parse():
     parser = argparse.ArgumentParser(prog="logreg_train")
@@ -31,15 +15,27 @@ def parse():
     parser.add_argument("-o", "--opti", help="the optimization algorithms")
     return parser.parse_args()
 
+def plot(loss_vec: list):
+    plt.plot(loss_vec)
+    plt.ylabel('Loss evolution')
+    plt.xlabel("number of iteration")
+    plt.show()
+
+
+def create_model(path : str):
+    thetas = np.array([[0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0]])
+
+    x, huff_y, gryf_y, sly_y, rav_y = load_data(path)
+
+    lr_gryf = lr(thetas)
+    lr_gryf.fit_(x, gryf_y)
+    plot(lr_gryf.historique)
 
 def main():
     args = parse()
     try:
-        df = load_data(args.dataset)
-        print(df)
-        print(df.describe())
-        #comme c'est un multi-classifier il faudra faire plusieur logistic regression
-
+        create_model(args.dataset)
+        
     except Exception as e:
         print(f"Error: {e}")
 

test.py
@@ -44,7 +44,7 @@ def plot(loss_vec: list):
 
 X = np.array([[1., 1., 2., 3.], [5., 8., 13., 21.], [3., 5., 9., 14.]])
 Y = np.array([[1], [0], [1]])
-thetas = np.array([[2], [0.5], [7.1], [-4.3], [2.09]])
+thetas = np.array([[0], [0], [0], [0], [0], [0], [0], [0], [0], [0], [0]])
 mylog = lr(thetas, algo='SGD', batch_size=2)
 # Example 0:
 y_hat = mylog.log_predict_(X)