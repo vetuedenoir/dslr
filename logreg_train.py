16fcab995a43c3debc92f19ac4852aab5a02bd7b
Killian SCORDEL <kscordel@f6r11s7.clusters.42paris.fr>
2026-02-06 18:34:03 +0100

    amelioration du main, entrainement des 4 models en paralel

--- logreg_train.py
+++ logreg_train.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 import argparse
+from concurrent.futures import ProcessPoolExecutor
+from itertools import repeat
 
 from LogisticRegression import LogisticRegression as lr
 from load_data import load_data
@@ -10,44 +12,67 @@ from load_data import load_data
 def parse():
     parser = argparse.ArgumentParser(prog="logreg_train")
     parser.add_argument("dataset", type=str, help="the dataset to train the model")
-    parser.add_argument("-p", "--plot", help="plot the evolution of loss function")
-    parser.add_argument("-a", "--algo", help="the algorithms to minimize the error")
-    parser.add_argument("-o", "--opti", help="the optimization algorithms")
+    parser.add_argument("-p", "--plot", action='store_true', help="plot the evolution of loss function")
+    parser.add_argument("-a", "--algo", type=str, help="the algorithms to minimize the error")
     return parser.parse_args()
 
-def plot(loss_vec: list):
+def plot(loss_vec: list, title: str):
     plt.plot(loss_vec)
     plt.ylabel('Loss evolution')
     plt.xlabel("number of iteration")
+    plt.title(title)
     plt.show()
 
+def train_model(x: np.ndarray , y: np.ndarray, thetas: np.ndarray, algo: str, p: bool, house_name: str):
+    model = lr(thetas.copy(), max_iter=300, alpha=0.01, algo=algo)
+    model.fit_(x, y)
+    print(f"{house_name} loss final : {model.log_loss_(y, model.log_predict_(x))}")
 
-def create_model(path : str):
+    if p == True:
+        plot(model.historique, house_name)
+    return model
+
+def save_thetas(models_thetas: list):
+    house = ["Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"]
+    houses_thetas = {}
+    houses_thetas["Gryffindor"] = models_thetas[0]
+    houses_thetas["Ravenclaw"] = models_thetas[1]
+    houses_thetas["Hufflepuff"] = models_thetas[2]
+    houses_thetas["Slytherin"] = models_thetas[3]
+
+    with open("assets/tethas.csv", "a") as file:
+        for house in houses_thetas:
+            line = house + "\t"
+            for theta in houses_thetas[house]:
+                line += f", {theta}"
+            file.write(line + "\n")
+
+
+def create_model(path : str, algo: str, plot: bool):
     thetas = np.array([[0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0]])
 
     x, huff_y, gryf_y, sly_y, rav_y = load_data(path)
+    house = ["Gryffindor", "Hufflepuff", "Slytherin", "Ravenclaw"]
 
-    lr_gryf = lr(thetas, max_iter=5000, alpha=0.01)
-    lr_gryf.fit_(x, gryf_y)
-    plot(lr_gryf.historique)
+    with ProcessPoolExecutor() as executor:
+        lr_gryf, lr_huff, lr_sly, lr_rav = executor.map(
+            train_model,
+            repeat(x),
+            [gryf_y, huff_y, sly_y, rav_y],
+            repeat(thetas),
+            repeat(algo),
+            repeat(plot),
+            house
+    )
 
-    # lr_huff = lr(thetas, max_iter=30000)
-    # lr_huff.fit_(x, huff_y)
-
-    # lr_sly = lr(thetas, max_iter=30000)
-    # lr_sly.fit_(x, sly_y)
-
-    # lr_rav = lr(thetas, max_iter=30000)
-    # lr_rav.fit_(x, rav_y)
-
-    # with open("assets/tethas.csv", "w") as file:
-    #     file.write("Ravenclaw : " + lr_rav.theta)
+    save_thetas([lr_gryf.theta, lr_rav.theta, lr_huff.theta, lr_sly.theta])
 
 def main():
     args = parse()
+    if args.algo is None:
+        args.algo = "gradient_descent"
     try:
-        create_model(args.dataset)
-        
+        create_model(args.dataset, args.algo, args.plot)     
     except Exception as e:
         print(f"Error: {e}")
 
